14f5e36f931478b21e59c0a11548b28fc1c6e3d3
kristinabog <kristinabog7@hotmail.com>
2022-11-16 20:18:36 +0200

    added method to save playlist to user account

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,6 +21,7 @@ class App extends React.Component{
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -44,6 +45,11 @@ class App extends React.Component{
     this.setState({ playlistName: name });
   }
 
+  savePlaylist() {
+    alert('this works');
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return(
       <div>
@@ -55,7 +61,8 @@ class App extends React.Component{
             <Playlist playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
-              onNameChange={this.updatePlaylistName} />
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
